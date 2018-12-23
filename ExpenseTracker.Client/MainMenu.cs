97dd266682de9fd28d81aa25879dcfcc42a9d3ac
Todor Mitskovski <todorbm85@gmail.com>
2018-12-23 14:28:33 +0200

    Refacor

--- ExpenseTracker.Client/MainMenu.cs
+++ ExpenseTracker.Client/MainMenu.cs
@@ -22,39 +22,78 @@ namespace ExpenseTracker.ConsoleClient
             while (response != "e")
             {
                 Console.WriteLine(@"
-im: imports From GMail
 ex: excel menu
 c: categories menu
-cl: classify all expenses
 q: query menu
+
+im: import GMail
+cl: classify all expenses
 b: backup database
+
 e: end");
 
                 response = Console.ReadLine();
                 switch (response)
                 {
                     case "im":
-                        ImportGmail();
+                        this.ImportGmail();
                         break;
+
                     case "ex":
                         new ExcelMenu(this.service).Run();
-                        break;                    
+                        break;
+
                     case "c":
                         new CategoriesMenu(this.GetRepo()).Run();
                         break;
+
                     case "cl":
-                        Categorize();
+                        this.Categorize();
                         break;
+
                     case "q":
                         new QueryMenu(this.service).Run();
                         break;
+
                     case "b":
-                        Utils.BackupFile(GetDbpath());
+                        BackupFile(GetDbpath());
                         break;
+
                     default:
                         break;
                 }
             }
+        }
+
+        private static void BackupFile(string sourcePath)
+        {
+            var rootPath = Path.GetDirectoryName(sourcePath);
+            var baseFileName = Path.GetFileNameWithoutExtension(sourcePath);
+            var extension = Path.GetExtension(sourcePath);
+
+            int i = 0;
+            var fileName = baseFileName + "_" + i;
+            var newPath = rootPath + "\\" + fileName + "." + extension;
+            while (File.Exists(newPath))
+            {
+                i++;
+                fileName = baseFileName + "_" + i;
+                newPath = rootPath + "\\" + fileName + "." + extension;
+            }
+
+            File.Copy(sourcePath, newPath);
+        }
+
+        private static string GetDbpath()
+        {
+            var path = ConfigurationManager.AppSettings["dbPath"];
+            path = Environment.ExpandEnvironmentVariables(path);
+            if (string.IsNullOrEmpty(path))
+            {
+                throw new ArgumentNullException("Enter valid db path:");
+            }
+
+            return path;
         }
 
         private void ImportGmail()
@@ -106,18 +145,6 @@ e: end");
             string path = GetDbpath();
 
             return new UnitOfWork(path);
-        }
-
-        private static string GetDbpath()
-        {
-            var path = ConfigurationManager.AppSettings["dbPath"];
-            path = Environment.ExpandEnvironmentVariables(path);
-            if (string.IsNullOrEmpty(path))
-            {
-                throw new ArgumentNullException("Enter valid db path:");
-            }
-
-            return path;
         }
 
         private void Categorize()
