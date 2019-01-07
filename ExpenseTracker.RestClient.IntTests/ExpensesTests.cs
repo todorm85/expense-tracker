eebc97413e77b5b7d80bc2e4f084cad41d6956b9
Todor Mitskovski <todorbm85@gmail.com>
2019-01-07 06:50:12 +0200

    Fix tests build

--- ExpenseTracker.RestClient.IntTests/ExpensesTests.cs
+++ ExpenseTracker.RestClient.IntTests/ExpensesTests.cs
@@ -8,17 +8,19 @@ namespace ExpenseTracker.RestClient.IntTests
     [TestClass]
     public class ExpensesTests
     {
+        private const string baseAddress = "https://localhost:44376";
+
         [TestMethod]
         public void Expenses_Get()
         {
-            var service = new ExpensesRestClient(new HttpClient() { BaseAddress = new Uri("https://localhost:44376") });
+            var service = new ExpensesRestClient(new RestHttpClient(baseAddress));
             var result = service.GetExpensesByCategoriesByMonths(DateTime.Now.AddYears(-1), DateTime.MaxValue);
         }
 
         [TestMethod]
         public void Expenses_Post()
         {
-            var service = new ExpensesRestClient(new HttpClient() { BaseAddress = new Uri("https://localhost:44376") });
+            var service = new ExpensesRestClient(new RestHttpClient(baseAddress));
             service.Add(new Expense[] 
             {
                 new Expense()
