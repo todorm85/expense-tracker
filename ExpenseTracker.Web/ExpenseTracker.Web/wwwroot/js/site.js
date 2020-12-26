45c97e0b9146a636ae33e35e958b6b2c811b89be
Todor <todorbm85@gmail.com>
2020-12-26 11:13:34 +0200

    Refactor, extact common post ajax logic

--- ExpenseTracker.Web/ExpenseTracker.Web/wwwroot/js/site.js
+++ ExpenseTracker.Web/ExpenseTracker.Web/wwwroot/js/site.js
@@ -14,3 +14,27 @@ function onToggleElement(elId) {
 document.querySelectorAll(".auto-select").forEach(e => {
     e.onfocus = ev => { ev.srcElement.select();}
 })
+
+function postAjax(address, onSuccess, onFail, data) {
+    var xhttp = new XMLHttpRequest();
+    xhttp.onreadystatechange = function () {
+        if (this.readyState == 4) {
+            if (this.status == 200) {
+                onSuccess(this);
+            } else {
+                onFail(this);
+            }
+        }
+    };
+
+    xhttp.open("POST", address, true);
+    xhttp.setRequestHeader("Content-type", "application/json");
+    xhttp.setRequestHeader("XSRF-TOKEN", document.querySelector('input[name="__RequestVerificationToken"]').value);
+    if (data) {
+        data = JSON.stringify(data);
+    } else {
+        data = '';
+    }
+
+    xhttp.send(data);
+}
