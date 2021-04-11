2987fee0ebf0d6e36c2c7b642b0974c41cdbd110
Todor <todorbm85@gmail.com>
2021-04-11 05:54:54 +0300

    Update rei parser

--- ExpenseTracker.Tests/Unit/Integrations/RaifeissenParserTests.cs
+++ ExpenseTracker.Tests/Unit/Integrations/RaifeissenParserTests.cs
@@ -50,5 +50,24 @@ Sofia 1407,
             Assert.AreEqual("POKUPKA EDDYS na BAKE", result.Details);
             Assert.AreEqual(new DateTime(2020, 1, 28), result.Date);
         }
+
+        [TestMethod]
+        public void ValidMessageV3IsParsedCorrectly()
+        {
+            var parser = new RaiffeisenMessageParser();
+            var result = parser.Parse(new ExpenseMessage()
+            {
+                Subject = "Notification from RBBBG",
+                Body = @"Uvazhaemi g-ne/g-zho,
+Bihme iskali da Vi uvedomim za POKUPKA  96.73 BGN s Vashata  ****5806 v BGR pri SHELL PODUENE na 10.04.2021 13:55:32. Razpolagaema nalichnost po kartata 1113.36 BGN.
+S uvazhenie,
+Raiffeisenbank (Bulgaria) EAD
+Sofia 1407, blvd Nikola I.Vaptzarov  55
+070010000 (VIVACOM)  1721 (A1 i Telenor)"
+            });
+            Assert.AreEqual(96.73M, result.Amount);
+            Assert.AreEqual("POKUPKA SHELL PODUENE", result.Details);
+            Assert.AreEqual(new DateTime(2021, 4, 10), result.Date);
+        }
     }
 }
