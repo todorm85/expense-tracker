cb471e633a1990c8e03c44febcb23a0131ff56b5
Todor Mitskovski <todorbm85@gmail.com>
2022-10-15 23:16:47 +0300

    Marked edit amount + refactor

---
+++ ExpenseTracker.Web/TagHelpers/ApplyButtonTagHelper.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Razor.TagHelpers;
+
+namespace ExpenseTracker.Web.TagHelpers
+{
+    [HtmlTargetElement(Attributes = "btn-apply")]
+    public class ApplyButtonTagHelper : TagHelper
+    {
+        public override void Process(TagHelperContext context, TagHelperOutput output)
+        {
+            output.Attributes.Add("class", "btn");
+            output.Attributes.Add("style", "width: 30px; height:30px; padding:3px; margin-right: 15px; font-size: 20px;");
+            output.Content.SetHtmlContent("&#10004;");
+            output.TagMode = TagMode.StartTagAndEndTag;
+        }
+    }
+}
