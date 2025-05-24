ecbaa99570aca16564fc6dbb2339c2c2ecc9a3ad
Todor Mitskovski <todorbm85@gmail.com>
2025-05-24 21:03:35 +0300

    Refactor VS Code tasks: rename "Ensure debug port" to "Free debug port" and update command to stop processes using the debug port; remove obsolete deployment and process stopping scripts.

--- scripts/deploy.ps1
+++
@@ -1,5 +0,0 @@
-Param($workspaceFolder, $destination)
-
-Remove-Item -Path "$destination*" -Recurse -Force -ErrorAction SilentlyContinue
-
-Copy-Item -Path "$workspaceFolder/ExpenseTracker.Web/bin/Release/net8.0/win-x64/publish/*" -Destination $destination -Recurse -Force
