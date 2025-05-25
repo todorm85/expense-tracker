47708f7b5ca87178322c56a4e45a9635b750bf36
Todor Mitskovski <todorbm85@gmail.com>
2025-05-26 00:09:32 +0300

    Refactor TransactionsFilter and add FormGroupTagHelper: streamline form layout with new tag helper and enhance category expression help section

---
+++ ExpenseTracker.Web/TagHelpers/FormGroupTagHelper.cs
@@ -0,0 +1,48 @@
+using Microsoft.AspNetCore.Razor.TagHelpers;
+
+namespace ExpenseTracker.Web.TagHelpers
+{
+    [HtmlTargetElement("form-group", TagStructure = TagStructure.NormalOrSelfClosing)]
+    public class FormGroupTagHelper : TagHelper
+    {
+        public bool Inline { get; set; } = true;
+        public string Class { get; set; } = "";
+
+        public override void Process(TagHelperContext context, TagHelperOutput output)
+        {
+            output.TagName = "div";
+            var classes = "form-group";
+            
+            if (Inline)
+            {
+                classes += " d-inline-block mr-2";
+            }
+
+            if (!string.IsNullOrEmpty(Class))
+            {
+                classes += " " + Class;
+            }
+
+            output.Attributes.SetAttribute("class", classes);
+
+            // Add classes to child elements
+            string labelClasses = "'mb-0', 'mr-1'";
+            string inputClasses = "'form-control', 'form-control-sm'";
+
+            if (Inline)
+            {
+                labelClasses += ", 'd-inline-block'";
+                inputClasses += ", 'w-auto', 'd-inline-block'";
+            }
+
+            output.PostElement.SetHtmlContent($@"<script>
+                var el = document.currentScript.previousElementSibling;
+                el.querySelector('label').classList.add({labelClasses});
+                var input = el.querySelector('input, select');
+                if(input) {{
+                  input.classList.add({inputClasses});
+                }}
+                </script>");
+        }
+    }
+}
